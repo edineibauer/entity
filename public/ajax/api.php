0152432aa2a5c68ea2574725053320ffc9f3ab20
Edinei <galera.org@gmail.com>
2018-11-22 11:40:37 -0200

    ajax files

--- public/ajax/api.php
+++ public/ajax/api.php
@@ -3,5 +3,7 @@
 $entity = filter_input(INPUT_POST, 'entity', FILTER_DEFAULT);
 $dados = filter_input(INPUT_POST, 'data', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
 
-$id = \Entity\Entity::add($entity, $dados);
-$data['data'] = $id;
+if(file_exists(PATH_HOME . "entity/cache/{$entity}.json")) {
+    $id = \Entity\Entity::add($entity, $dados);
+    $data['data'] = $id;
+}
---
+++ public/ajax/delete/entity.php
@@ -0,0 +1,10 @@
+<?php
+
+$entity = filter_input(INPUT_POST, 'entity', FILTER_DEFAULT);
+$id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
+
+$result = \Entity\Entity::delete($entity, $id);
+$data['data']['data'] = $result;
+
+$json = new \Entity\Json();
+$data['data']['historic'] = $json->get("historic")[$entity];
