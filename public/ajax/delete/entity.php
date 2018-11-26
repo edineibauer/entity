020a17e15276e60bbb44f9f8b3acdf50e4662b65
Edinei <galera.org@gmail.com>
2018-11-26 11:10:10 -0200

    update entitys responses ajax

--- public/ajax/delete/entity.php
+++ public/ajax/delete/entity.php
@@ -3,7 +3,8 @@
 $entity = filter_input(INPUT_POST, 'entity', FILTER_DEFAULT);
 $id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
 
-$result = \Entity\Entity::delete($entity, $id);
+$result = \Entity\Entity::delete($entity, ['id' => $id]);
+$data['data'] = [];
 $data['data']['data'] = $result;
 
 $json = new \Entity\Json();
--- public/ajax/up/entity.php
+++ public/ajax/up/entity.php
@@ -5,6 +5,7 @@ $dados = filter_input(INPUT_POST, 'dados', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY)
 
 if(file_exists(PATH_HOME . "entity/cache/{$entity}.json")) {
     $id = \Entity\Entity::add($entity, $dados);
+    $data['data'] = [];
     $data['data']['data'] = $id;
 
     $json = new \Entity\Json();
